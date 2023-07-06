8cc0ce048417988f7ded47ab29d85a978d30ba46
JinxXP <63327885+JinxXP@users.noreply.github.com>
2023-07-05 22:59:35 -0400

    created puck.js

---
+++ Animation Lab/Puck/js/puck.js
@@ -0,0 +1,27 @@
+/*
+    N220 Section 5946
+    Antonio Townsend
+    Puck Side
+    5 July 2023
+*/
+
+function setup() {
+    createCanvas(400, 300);
+
+}//end setup
+
+//size of cursor circle
+var diameter = 20;
+    
+function draw() {
+    background("grey");
+    
+    if(mouseX>200){ //to the right of center screen
+        fill("red");
+        circle(mouseX,mouseY,diameter);
+    }else{  //to the left of center screen
+        fill("blue");
+        circle(mouseX,mouseY,diameter);
+    }//end if
+
+}//end draw
