bc3f9ba5d7c07f897275cdc5527af2610762bcae
JinxXP <63327885+JinxXP@users.noreply.github.com>
2023-07-07 21:38:46 -0400

    created for loop

--- Loop Exercises/Pyramid/js/pyramid.js
+++ Loop Exercises/Pyramid/js/pyramid.js
@@ -11,9 +11,23 @@ const pyramid = 25;
 //create background canvas
 function setup() {
     createCanvas(800,600);
+    background("white");
 //colors for each square and stroke() for borders
     fill("red");
 
     stroke("white");
 
+}//end function
+
+function draw() {
+
+var i = 0; 
+var j = 0; 
+//for loop that'll loop 4 times
+    for(i = 0; i < 4; i++) {
+    //nested loop. shape will be drawn here
+        for(j = 0; j <= i; j++) {
+            rect(pyramid * j, pyramid * i, pyramid, pyramid);  //x, y, h all the same
+        }//end for j
+    }//end for i
 }//end function
