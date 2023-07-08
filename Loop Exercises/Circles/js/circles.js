6ceab508ff8dd3ba87bbc4777cca9992233a4344
JinxXP <63327885+JinxXP@users.noreply.github.com>
2023-07-07 20:06:07 -0400

    created functions in circles.js

--- Loop Exercises/Circles/js/circles.js
+++ Loop Exercises/Circles/js/circles.js
@@ -4,3 +4,13 @@
     Circles inside of Circles
     7 July 2023
 */
+
+//create background canvas
+function setup() {
+    createCanvas(800, 600);
+    background("#00E3E0");
+}//end function
+
+function draw() {
+
+}//end function
