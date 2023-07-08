5d3c3b57dc9bcd3da4acb720a653e03c57e60c1b
JinxXP <63327885+JinxXP@users.noreply.github.com>
2023-07-07 21:29:31 -0400

    created setup function

--- Loop Exercises/Pyramid/js/pyramid.js
+++ Loop Exercises/Pyramid/js/pyramid.js
@@ -4,3 +4,16 @@
     Pyramid Lab
     7 July 2023
 */
+
+//const size of each square
+const pyramid = 25;
+
+//create background canvas
+function setup() {
+    createCanvas(800,600);
+//colors for each square and stroke() for borders
+    fill("red");
+
+    stroke("white");
+
+}//end function
