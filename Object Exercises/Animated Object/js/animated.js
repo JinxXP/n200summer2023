ef3e37c5f73c59da1134b3f576a253b9f0e28b3a
JinxXP <imusic0512@gmail.com>
2023-07-18 20:20:46 -0400

    created setup function

--- Object Exercises/Animated Object/js/animated.js
+++ Object Exercises/Animated Object/js/animated.js
@@ -4,3 +4,19 @@
     Neopolitan
     18 July 2023
 */
+
+//define circle object globally
+let circle;
+
+//create canvas dimensions and circle properties
+function setup() {
+    createCanvas(800, 600);
+
+    circle = {
+        x:  100,
+        y:  100,
+        size: 40
+    };//end circle
+
+}//end function
+
