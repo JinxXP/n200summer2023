c63c9f1299db7a5748ad1e0aa8217ba21fa87d78
JinxXP <imusic0512@gmail.com>
2023-07-22 17:07:55 -0400

    created enemy color

--- Midterm/js/app.js
+++ Midterm/js/app.js
@@ -4,33 +4,33 @@
     Midterm Exam
     22 July 2023
 */
-
-
-//create canvas dimensions and player and enemy properties
-function setup() {
-    createCanvas(800, 800);
-
+//properties for player
     player = {
         x : 400,
         y : 150,
         size: 40
     }//end player
 
-
 //enemy coords will vary as there's going to be several enemies on screen
 let enemy = ["enemy1", "enemy2", "enemy3", "enemy4"];
+//let enemyColor = "red";
+//create canvas dimensions and player and enemy properties
+function setup() {
+    createCanvas(1000, 800);
 
 }//end function
 
 function draw() {
     //1st row of enemies
     for(var i=0; i < enemy.length; i++) {  //.length will go through and draw a circle for each enemy within the array
+        fill("red");
         circle(200, 100, 40);
         circle(400, 100, 40);
         circle(600, 100, 40);
         circle(800, 100, 40);
 
     } //end for
+
 
 
 }//end function 
