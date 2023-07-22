df3171e2400bc64f2d4aa3762928bec689dd8068
JinxXP <imusic0512@gmail.com>
2023-07-22 16:44:48 -0400

    created enemy and player variables

--- Midterm/js/app.js
+++ Midterm/js/app.js
@@ -5,7 +5,21 @@
     22 July 2023
 */
 
-//create canvas dimensions
-function setup() 
+
+//create canvas dimensions and player and enemy properties
+function setup() {
     createCanvas(800, 800);
+
+    player = {
+        x : 400,
+        y : 150,
+        size: 40
+    }//end player
+
+    enemy = {   //coords will vary as there's going to be several enemies on screen
+        size : 40
+    }//end enemy
+
+}//end function
+
 
