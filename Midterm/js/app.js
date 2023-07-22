8e7a70d50c349a842ad6dbc08f6dfc23de799845
JinxXP <imusic0512@gmail.com>
2023-07-22 17:17:01 -0400

    removed player object

--- Midterm/js/app.js
+++ Midterm/js/app.js
@@ -4,19 +4,13 @@
     Midterm Exam
     22 July 2023
 */
-//properties for player
-    player = {
-        x : 400,
-        y : 150,
-        size: 40
-    }//end player
 
 //enemy coords will vary as there's going to be several enemies on screen
 let enemy = ["enemy1", "enemy2", "enemy3", "enemy4"];
 //let enemyColor = "red";
 //create canvas dimensions and player and enemy properties
 function setup() {
-    createCanvas(1000, 800);
+    createCanvas(1000, 600);
 
 }//end function
 
@@ -31,7 +25,10 @@ function draw() {
 
     } //end for
 
+//create player
+player = circle(400, 550, 40);
 
+fill("blue");
 
 }//end function 
 
