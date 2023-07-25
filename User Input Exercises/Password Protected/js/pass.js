5a50943fed4571fc627f1f41f790cacc2b0144bb
JinxXP <imusic0512@gmail.com>
2023-07-25 17:57:51 -0400

    created variables

--- User Input Exercises/Password Protected/js/pass.js
+++ User Input Exercises/Password Protected/js/pass.js
@@ -5,3 +5,12 @@
     Password Protected
     25 July 2023
 */
+
+//function that checks for correct username and password
+function login() {
+    //username field
+    let user=document.getElementById("username").value;
+
+    //password field
+    let userPass=document.getElementById("password").value;
+}
