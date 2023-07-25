0ee0b4f39edc759d73fafd13d7c7d8a2f7d9ac76
JinxXP <imusic0512@gmail.com>
2023-07-25 17:39:36 -0400

    created calculation to tipCalc.js

--- User Input Exercises/Tip Calculator/js/tipCalc.js
+++ User Input Exercises/Tip Calculator/js/tipCalc.js
@@ -4,3 +4,17 @@
     Tip Calculator
     25 July 2023
 **/
+
+
+//function for tip calc
+function calculate() {
+    let userInput=document.getElementById("subtotal").value;
+
+//tip calulation is 10%
+    let tip = userInput * 0.10;
+
+//adding tip to subtotal amount
+    let total = parseInt(userInput) + parseInt(tip);
+
+
+}
