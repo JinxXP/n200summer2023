94d6c47a14cd1fc5f4e96ea8a43bf0fca2a4b11a
JinxXP <imusic0512@gmail.com>
2023-07-28 17:04:08 -0400

    updated js

--- Function Exercises/Divisible by 7/js/seven.js
+++ Function Exercises/Divisible by 7/js/seven.js
@@ -5,3 +5,15 @@
     28 July 2023
 */
 
+function divide() {
+    let x = document.getElementById("response").value;
+
+    if (x%7==0) {   //use % to find remainder  
+        document.getElementById("output").innerHTML="Number is divisible."
+        return true;
+    }else {
+        document.getElementById("output").innerHTML="Number is NOT divisible."
+        return false;
+    }//end else
+
+}//end function
