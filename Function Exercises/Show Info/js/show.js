a0d454f3fbe99d50fdb1911b3294a0d5c75d6eee
JinxXP <imusic0512@gmail.com>
2023-07-28 17:25:29 -0400

    Update show.js

--- Function Exercises/Show Info/js/show.js
+++ Function Exercises/Show Info/js/show.js
@@ -5,3 +5,18 @@
     Show Info
     28 July 2023
 */
+
+let contentButtons = ['Home is located here', 'About is located here', 'Art is located here'];
+
+function displayContent(arrayIndex) {
+    let output = document.getElementById("output");
+
+    //checks array for matching index
+    if (arrayIndex >= 0 && arrayIndex < contentButtons.length) {
+        output.innerHTML = contentButtons[arrayIndex];
+
+    }else{
+        output.innerHTML = "Nothing is here";
+    }//end else
+
+}//end function
