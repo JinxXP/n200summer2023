e66577e00d3cfed1f10f3c53abc761030c42fdcd
JinxXP <imusic0512@gmail.com>
2023-07-28 17:46:48 -0400

    Update click.js

--- Function Exercises/Click for Random/js/click.js
+++ Function Exercises/Click for Random/js/click.js
@@ -4,3 +4,24 @@
     Click for Random
     28 July 2023
 */
+
+//define color placeholders
+let colorArray = [c, m, y, k] 
+
+function setup() {
+    createCanvas(800, 600);
+    c = random(255);
+    m = random(255);
+    y = random(255);
+    k = random(255);
+
+}//end function
+
+function draw() {
+    //properties for circle
+    stroke(c, m, y, k);
+    fill(c, m, y, k);
+    circle(400, 300, 50);
+}//end function
+
+
